c8010dbca7b4902924b0bf931b61c0bec825b44a
Adam Łastowski <adam.lastowski@mail.com>
2015-01-07 11:47:31 +0100

    Initial commit

---
+++ app/src/androidTest/java/co/pilot/citest/MainActivityTest.java
@@ -0,0 +1,57 @@
+package co.pilot.citest;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by adamlastowski on 07.01.15.
+ */
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    Activity mActivity;
+    Button mButton;
+    TextView mTextView;
+
+    Instrumentation mInstrumentation;
+
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        mActivity = getActivity();
+
+        mInstrumentation = getInstrumentation();
+
+        mButton = (Button) mActivity.findViewById(R.id.btn_toggle);
+        mTextView = (TextView) mActivity.findViewById(R.id.textView);
+
+        super.setUp();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testIfViewsAreInitializedProperly() throws Exception {
+        assertNotNull(mButton);
+        assertNotNull(mTextView);
+    }
+
+    public void testTextViewDisplayingCorrectText() throws Exception {
+        assertEquals("Start", mTextView.getText().toString());
+    }
+
+    public void testButtonToggleOneTime() throws Exception {
+        mButton.callOnClick();
+
+        mInstrumentation.waitForIdleSync();
+
+        assertEquals("End", mTextView.getText().toString());
+    }
+}
---
+++ app/src/main/java/co/pilot/citest/MainActivity.java
@@ -0,0 +1,58 @@
+package co.pilot.citest;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+    Button mBtnToggle;
+    TextView mTvTarget;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBtnToggle = (Button) findViewById(R.id.btn_toggle);
+        mTvTarget = (TextView) findViewById(R.id.textView);
+
+        mBtnToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mTvTarget.getText().toString().equals("Start")){
+                    mTvTarget.setText("End");
+                } else {
+                    mTvTarget.setText("Start");
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
