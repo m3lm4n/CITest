959a003e53a3ba4bce51026c8d5eb4d5571cff50
Adam Łastowski <m3lm4n@gmail.com>
2015-01-08 10:12:51 +0100

    Dependencies bash

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -12,5 +12,8 @@ if [ ! -e $DEPS ]; then
   echo y | android update sdk -u -a -t build-tools-21.1.1 &&
   echo y | android update sdk -u -a -t sys-img-x86-android-19 &&
   echo n | android create avd -n testing -f -t android-19 &&
+  echo y | android update sdk --filter "extra-android-m2repository" --no-ui  
+  echo y | android update sdk --filter "extra-android-support" --no-ui  
+  echo y | android update sdk --filter "extra-google-m2repository" --no-ui
   touch $DEPS
 fi
